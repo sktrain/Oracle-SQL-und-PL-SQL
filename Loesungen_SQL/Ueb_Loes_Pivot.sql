d5899000c98ffc54cfe2f180b95f3a1a3dda4332
sktrain <39360116+sktrain@users.noreply.github.com>
2023-10-26 05:48:23 +0200

    Add files via upload

---
+++ Loesungen_SQL/Ueb_Loes_Pivot.sql
@@ -0,0 +1,142 @@
+ /* 
+ The PIVOT operator takes data in separate rows, aggregates it and converts it 
+ into columns. 
+ To see the PIVOT operator in action we need to create a test table.
+ */
+ 
+ create table pivot_test (
+  id            number,
+  customer_id   number,
+  product_code  varchar2(5),
+  quantity      number
+);
+
+insert into pivot_test values (1, 1, 'A', 10);
+insert into pivot_test values (2, 1, 'B', 20);
+insert into pivot_test values (3, 1, 'C', 30);
+insert into pivot_test values (4, 2, 'A', 40);
+insert into pivot_test values (5, 2, 'C', 50);
+insert into pivot_test values (6, 3, 'A', 60);
+insert into pivot_test values (7, 3, 'B', 70);
+insert into pivot_test values (8, 3, 'C', 80);
+insert into pivot_test values (9, 3, 'D', 90);
+insert into pivot_test values (10, 4, 'A', 100);
+commit;
+
+
+/* Aufgabe: wir wollen folgendes Ergebnis:
+   Eine Zeile mit den aufsummierten Quantitys
+   
+A_SUM_QUANTITY B_SUM_QUANTITY C_SUM_QUANTITY
+-------------- -------------- --------------
+           210             90            160
+           
+*/
+select *
+from   (select product_code, quantity
+        from   pivot_test)
+pivot  (sum(quantity) as sum_quantity for (product_code)
+        in ('A' as a, 'B' as b, 'C' as c));
+        
+
+        
+/* Aufgabe: jetzt pro Customer
+   
+CUSTOMER_ID A_SUM_QUANTITY B_SUM_QUANTITY C_SUM_QUANTITY
+----------- -------------- -------------- --------------
+          1             10             20             30
+          2             40                            50
+          3             60             70             80
+          4            100
+           
+*/
+select *
+from   (select customer_id, product_code, quantity
+        from   pivot_test)
+pivot  (sum(quantity) as sum_quantity for (product_code) 
+        in ('A' as a, 'B' as b, 'C' as c))
+order by customer_id;
+
+
+/* Aufgabe: jetzt mal mit Hilfe von XML und ANY für alle Products im
+   XML-Format (Dadurch haben wir dynamisch alle Products pro Customer)
+   
+product_code_XML
+----------------------------------------------------------------------------------------------------
+<PivotSet><item><column name = "PRODUCT_CODE">A</column><column name = "SUM_QUANTITY">210</column></
+item><item><column name = "PRODUCT_CODE">B</column><column name = "SUM_QUANTITY">90</column></item><
+item><column name = "PRODUCT_CODE">C</column><column name = "SUM_QUANTITY">160</column></item><item>
+<column name = "PRODUCT_CODE">D</column><column name = "SUM_QUANTITY">90</column></item></PivotSet>
+           
+*/
+
+select *
+from   (select customer_id, product_code, quantity
+        from   pivot_test)
+pivot xml (sum(quantity) as sum_quantity for (product_code) in (ANY));
+
+-- oder auch
+select *
+from   (select customer_id, product_code, quantity
+        from   pivot_test)
+pivot xml (sum(quantity) as sum_quantity for (product_code) 
+        in (select distinct product_code from  pivot_test)); 
+        
+
+
+/* Für den UNPIVOT nutzen wir folgende Tabelle */
+
+create table unpivot_test (
+  id              number,
+  customer_id     number,
+  product_code_a  number,
+  product_code_b  number,
+  product_code_c  number,
+  product_code_d  number
+);
+
+insert into unpivot_test values (1, 101, 10, 20, 30, null);
+insert into unpivot_test values (2, 102, 40, null, 50, null);
+insert into unpivot_test values (3, 103, 60, 70, 80, 90);
+insert into unpivot_test values (4, 104, 100, null, null, null);
+commit;
+
+
+/* Aufgabe: Mit Hilfe des UNPIVOT-Operator wollen wir folgendes Ergebnis:
+
+        ID CUSTOMER_ID P   QUANTITY
+---------- ----------- - ----------
+         1         101 A         10
+         1         101 B         20
+         1         101 C         30
+         2         102 A         40
+         2         102 C         50
+         3         103 A         60
+         3         103 B         70
+         3         103 C         80
+         3         103 D         90
+         4         104 A        100
+
+*/
+
+select *
+from   unpivot_test
+unpivot (quantity for product_code in 
+             (product_code_a as 'A', 
+              product_code_b as 'B', 
+              product_code_c as 'C', 
+              product_code_d as 'D'));
+              
+
+/* Aufgabe: Zusätzlich sollen Nullwerte in den Aggregationsspalten des 
+   Ausgangstabelle im Ergebnis auch berücksichtigt werden
+*/
+
+select *
+from   unpivot_test
+unpivot include nulls (quantity for product_code in 
+             (product_code_a as 'A', 
+              product_code_b as 'B', 
+              product_code_c as 'C', 
+              product_code_d as 'D'));
+              
