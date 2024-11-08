5a44e3609df47f657e01506438abf438252d36c7
sktrain <39360116+sktrain@users.noreply.github.com>
2024-11-08 10:01:43 +0100

    Update Ueb_DML.sql

--- Uebungen_SQL/Ueb_DML.sql
+++ Uebungen_SQL/Ueb_DML.sql
@@ -1,8 +1,13 @@
--- Übungen/Lösungen zu DML
+-- Ãœbungen/LÃ¶sungen zu DML
 
 -- 1. Erzeugen Sie mit Hilfe der folgenden Anweisungen die Tabelle 
 -- "MY EMPLOYEE", die im Folgenden verwendet wird:
-
+CREATE TABLE MY_EMPLOYEE
+  ( id NUMBER(4) CONSTRAINT my_employee_id_nn NOT NULL, 
+    lastname VARCHAR2(25),
+    firstname VARCHAR2(25),
+    userid VARCHAR2(8),
+    salary NUMBER(9,2));
     
  
     
@@ -11,7 +16,7 @@
 
 
 
--- 3. Fügen sie die erste Zeile in die Tabelle MY_EMPLOYEE von den folgenden 
+-- 3. FÃ¼gen sie die erste Zeile in die Tabelle MY_EMPLOYEE von den folgenden 
 -- Beispielsdaten ein, ohne die Spalten  beim INSERT aufzulisten.
 -- ID   LAST_NAME   FIRST NAME    USERID    SALARY
 -- 1    Patel       Ralph         rpatel      895
@@ -23,40 +28,40 @@
 
 
 
--- 4. Füllen Sie die MY EMPLOYEE Tabelle mit der 2. und 3. Zeile der 
+-- 4. FÃ¼llen Sie die MY EMPLOYEE Tabelle mit der 2. und 3. Zeile der 
 -- Beispielsdaten, diemal mit expliziter Nennung der Spalten in der 
 -- INSERT-Klausel.
 
 
 
 
--- 5. Schreiben Sie die Änderungen fest.
+-- 5. Schreiben Sie die Ã„nderungen fest.
 
 
 
 
--- 6. Ändern Sie den Namen des Angestellten 3 zu Drexler.
+-- 6. Ã„ndern Sie den Namen des Angestellten 3 zu Drexler.
 
 
 
 
--- 7. Ändern Sie das Gehalt für alle Angestellten, die weniger als 900 
+-- 7. Ã„ndern Sie das Gehalt fÃ¼r alle Angestellten, die weniger als 900 
 -- verdienen, auf 1000.
 
 
 
 
--- 8. Löschen Sie Betty Dancs aus der Tabelle MY_EMPLOYEE.
+-- 8. LÃ¶schen Sie Betty Dancs aus der Tabelle MY_EMPLOYEE.
 
 
 
 
--- 9. Schreiben Sie alle anstehenden Änderungen fest.
+-- 9. Schreiben Sie alle anstehenden Ã„nderungen fest.
 
 
 
 
--- 10. Füllen Sie die MY_EMPLOYEE Tabelle mit der 4. Zeile der Beispielsdaten.
+-- 10. FÃ¼llen Sie die MY_EMPLOYEE Tabelle mit der 4. Zeile der Beispielsdaten.
 
 
 
@@ -71,11 +76,11 @@
 
 
 
--- 13. Machen Sie das Löschen rückgängig ohne die vorherige INSERT-Anweisung 
--- rückgängig zu machen. 
+-- 13. Machen Sie das LÃ¶schen rÃ¼ckgÃ¤ngig ohne die vorherige INSERT-Anweisung 
+-- rÃ¼ckgÃ¤ngig zu machen. 
 
 
 
 
--- 14. Schreiben Sie die Daten endgültig fest.
+-- 14. Schreiben Sie die Daten endgÃ¼ltig fest.
 
